72fd25569882ea84584cfc8da3dc41c89e7a2e85
alexhollier <167818018+alexhollier@users.noreply.github.com>
2024-10-31 17:50:27 -0500

    Updated Code

--- AVLTree.java
+++ AVLTree.java
@@ -1,110 +1,394 @@
 
 public class AVLTree {
-    
-    public class Node{
-        int key;
-        Node left;
-        Node right;
+
+    public class Node extends AVLTree {
+
+        /*	int orderID;
+		String title;
+		Node left, right;
+		int height;
+
+		Node(int orderID, String title) {
+			this.orderID = orderID;
+			this.title = title;
+			height = 1;
+		}
+	}
+
+	private Node x;
+
+	public int height(Node x) {
+		if (x == null)
+			return 0;
+		return x.height;
+	}
+
+	private int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+
+	public Node rightRotate(Node y) {
+		Node x = y.left;
+		Node temp = x.right;
+		x.right = y;
+		y.left = temp;
+
+		x.height = max(height(x.left), height(x.right)) + 1;
+		y.height = max(height(y.left), height(y.right)) + 1;
+
+		return x;
+	}
+
+	public Node leftRotate(Node x) {
+		Node y = x.right;
+		Node temp = y.left;
+		y.left = x;
+		x.right = temp;
+
+		x.height = max(height(x.left), height(x.right)) + 1;
+		y.height = max(height(y.left), height(y.right)) + 1;
+
+		return y;
+	}
+
+	public int getBalance(Node x) {
+		if (x == null)
+			return 0;
+		return height(x.left) - height(x.right);
+	}
+
+	public Node rebalance(Node x) {
+		int balanceFactor = getBalance(x);
+
+		if (balanceFactor < -1) {
+			if (getBalance(x.left) <= 0) {
+				x = rightRotate(x);
+			} else {
+				x.left = leftRotate(x.left);
+				x = rightRotate(x);
+			}
+		}
+		if (balanceFactor > 1) {
+			if (getBalance(x.right) >= 0) {
+				x = leftRotate(x);
+			} else {
+				x.right = rightRotate(x.right);
+				x = leftRotate(x);
+			}
+		}
+		return x;
+	}
+
+	public int updateHeight(Node x) {
+		if (x == null) {
+			return -1;
+		} else {
+			x.height = 1 + Math.max(updateHeight(x.left), updateHeight(x.right));
+		}
+		return x.height;
+	}
+
+	public Node insert(Node x, int orderID, String title) {
+		if (x == null) {
+			return (new Node(orderID, title));
+		}
+
+		if (orderID < x.orderID) {
+			x.left = insert(x.left, orderID, title);
+		} else if (orderID > x.orderID) {
+			x.right = insert(x.right, orderID, title);
+		} else {
+			return x;
+		}
+		updateHeight(x);
+		return rebalance(x);
+	}
+
+	public Node deleteNode(Node x, int orderID) {
+        if (x == null)
+            return x; // If tree is empty
+
+        // Perform standard BST delete
+        if (orderID < x.orderID)
+            x.left = deleteNode(x.left, orderID);
+        else if (orderID > x.orderID)
+            x.right = deleteNode(x.right, orderID);
+        else {
+            if ((x.left == null) || (x.right == null)) {
+                Node temp = (x.left != null) ? x.left : x.right;
+
+                if (temp == null) {
+                    temp = x;
+                    x = null;
+                } else
+                    x = temp;
+            } else {
+          
+                Node temp = minValueNode(x.right);
+
+                x.orderID = temp.orderID;
+                x.title = temp.title;
+
+                x.right = deleteNode(x.right, temp.orderID);
+            }
+        }
+
+        if (x == null)
+            return x;
+		updateHeight(x);
+
+		return rebalance(x);
+	}
+
+	public void inOrder(Node x) {
+		if (x != null) {
+			inOrder(x.left);
+			System.out.println("Order ID: " + x.orderID + ", Book: " + x.title); // Print current x
+			inOrder(x.right);
+		}
+	}
+
+	public String search(Node x, int orderID) {
+		if (x == null)
+			return "That order could not be found.";
+
+		if (orderID < x.orderID)
+			return search(x.left, orderID);
+		else if (orderID > x.orderID)
+			return search(x.right, orderID);
+		else
+			return x.title;
+	}
+
+	public Node minValueNode(Node x) {
+		Node current = x;
+		while (current.left != null)
+			current = current.left;
+		return current;
+	}
+
+	public Node findOldestOrder(Node x) {
+		return minValueNode(x);
+	}
+
+	public Node findLatestOrder(Node x) {
+		Node current = x;
+		while (current.right != null)
+			current = current.right;
+		return current;
+	}
+
+	public Node getRoot() {
+		return this.x;
+	}
+
+	public void setRoot(Node x) {
+		this.x = x;
+	}
+	
+	public static int countNodes(Node x) {
+		if(x == null)
+			return 0;
+		return 1 + countNodes(x.left)+ countNodes(x.right);
+	}
+	
+	public static int calculateHeight(Node x) {
+		return (int) Math.ceil(Math.log(countNodes(x) + 1) / Math.log(2));
+	}
+         */
+        int orderID;
+        String title;
+        Node left, right;
         int height;
-        
-        Node(int key){
-            this.key = key;
-            this.left = null;
-            this.right = null;
-            this.height = 1;
+
+        Node(int orderID, String title) {
+            this.orderID = orderID;
+            this.title = title;
+            height = 1;
         }
     }
-    //Node root;
-        public int height(Node x){
-            if(x == null)
-                return -1;
-            return x.height;
+
+    private Node x;
+
+    private int height(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.height;
+    }
+
+    private int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(y.left), height(y.right)) + 1;
+
+        return x;
+    }
+
+    private Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    private int getBalance(Node n) {
+        if (n == null) {
+            return 0;
+        }
+        return height(n.left) - height(n.right);
+    }
+
+    public Node insert(Node x, int orderID, String title) {
+        if (x == null) {
+            return (new Node(orderID, title));
+        }
+
+        if (orderID < x.orderID) {
+            x.left = insert(x.left, orderID, title);
+        } else if (orderID > x.orderID) {
+            x.right = insert(x.right, orderID, title);
+        } else {
+            return x;
+        }
+
+        x.height = 1 + max(height(x.left), height(x.right));
+        updateHeight(x);
+        return rebalance(x);
+
+    }
+
+    public void inOrder(Node x) {
+        if (x != null) {
+            inOrder(x.left);
+            System.out.println("Order ID: " + x.orderID + ", Book: " + x.title);
+            inOrder(x.right);
+        }
+    }
+
+    public String search(Node x, int orderID) {
+        if (x == null) {
+            return "Order not found";
+        }
+        if (orderID < x.orderID) {
+            return search(x.left, orderID); 
+        } else if (orderID > x.orderID) {
+            return search(x.right, orderID);
+        } else {
+            return x.title;
+        }
+    }
+
+    private Node minValueNode(Node x) {
+        Node current = x;
+        while (current.left != null) {
+            current = current.left; 
+        }
+        return current;
+    }
+
+    public Node deleteNode(Node x, int orderID) {
+        if (x == null) {
+            return x;
+        }
+        if (orderID < x.orderID) {
+            x.left = deleteNode(x.left, orderID);
+        } else if (orderID > x.orderID) {
+            x.right = deleteNode(x.right, orderID);
+        } else {
+            if ((x.left == null) || (x.right == null)) {
+                Node temp = (x.left != null) ? x.left : x.right;
+
+                if (temp == null) {
+                    temp = x;
+                    x = null;
+                } else {
+                    x = temp;
+                }
+            } else {
+                Node temp = minValueNode(x.right);
+
+                x.orderID = temp.orderID;
+                x.title = temp.title;
+
+                x.right = deleteNode(x.right, temp.orderID);
             }
-            
-        public Node rightRotate(Node x){
-            Node y = x.left;
-            Node temp = y.right;
-            y.right = x;
-            x.left = temp;
-            
-            return y;
         }
-        
-        public Node leftRotate(Node x){
-            Node y = x.right;
-            Node temp = y.left;
-            y.left = x;
-            x.right = temp;
-            
-            return y;
+
+        if (x == null) {
+            return x;
         }
-        
-        public int getBalance(Node x) {
-            if(x == null)
-                return 0;
-            return height(x.left) - height(x.right);
+        x.height = max(height(x.left), height(x.right)) + 1;
+        updateHeight(x);
+
+        return rebalance(x);
+    }
+    
+    public int updateHeight(Node x) {
+		if (x == null) {
+			return -1;
+		} else {
+			x.height = 1 + Math.max(updateHeight(x.left), updateHeight(x.right));
+		}
+		return x.height;
+	}
+
+    public Node rebalance(Node x){
+                int balance = getBalance(x);
+
+        if (balance > 1 && getBalance(x.left) >= 0) {
+            return rightRotate(x);
         }
-        
-        public Node rebalance(Node x) {
-        	int balanceFactor = getBalance(x);
-        	
-        	if (balanceFactor < -1) {
-        		if (getBalance(x.left) <= 0) {
-            		x = rightRotate(x);        			
-        		}
-        		else {
-        			x.left = leftRotate(x.left);
-        			x = rightRotate(x);
-        		}
-        	}
-        	if(balanceFactor > 1) {
-        		if(getBalance(x.right) >= 0) {
-        			x = leftRotate(x);
-        		}
-        		else {
-        			x.right = rightRotate(x.right);
-        			x = leftRotate(x);
-        		}
-        	}
-        	return x;
+
+        if (balance > 1 && getBalance(x.left) < 0) {
+            x.left = leftRotate(x.left);
+            return rightRotate(x);
         }
-        
-        public int updateHeight(Node x) {
-        	if (x == null) {
-        		return -1;
-        	}
-        	else {
-        		x.height = 1 + Math.max(updateHeight(x.left), updateHeight(x.right));
-        	}
-        	return x.height;
+
+        if (balance < -1 && getBalance(x.right) <= 0) {
+            return leftRotate(x);
         }
-        
-        public Node insertNode(int key, Node x) {
-        	x = insertNode(key, x);
-        	updateHeight(x);
-        	return rebalance(x);
+
+        if (balance < -1 && getBalance(x.right) > 0) {
+            x.right = rightRotate(x.right);
+            return leftRotate(x);
         }
-        
-        public Node deleteNode(int key, Node x) {
-        	x = deleteNode(key, x);
-        	if (x == null) { 
-        		return null;
-        	}
-        	updateHeight(x);
-        	
-        	return rebalance(x);
+
+        return x;
+    }
+    public Node findOldestOrder(Node x) {
+        return minValueNode(x);
+    }
+
+    public Node findLatestOrder(Node x) {
+        Node current = x;
+        while (current.right != null) {
+            current = current.right;
         }
-        
-        public void inOrderTraversal(Node x){
-        	if (x == null) {
-        		return;
-        	}
-        	
-        	inOrderTraversal(x.left);
-        	System.out.print(x.key + " ");
-        	inOrderTraversal(x.right);
-        } 
-        public void inOrder() {
-        	inOrderTraversal(root);
-        }
+        return current;
+    }
+
+    public Node getRoot() {
+        return this.x;
+    }
+
+    public void setRoot(Node x) {
+        this.x = x;
+    }
+
 }
--- BookOrder.java
+++ BookOrder.java
@@ -1,67 +1,128 @@
+
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 import java.util.Scanner;
 
+public class BookOrder extends AVLTree {
 
-public class BookOrder {
-	
+    public static AVLTree tree = new AVLTree();
 
-	public static void main(String[] args) throws Exception {
-		
-	Node test  = new Node(1);
-	Node test2 = new Node();
-		
-	BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\aholl91\\eclipse-workspace\\ProgAssign2\\src\\orders.csv"));
-	List<String> lines = new ArrayList<>();
-	String line = null;
-	while((line = reader.readLine()) != null) {
-		lines.add(line);
-	}
-	
-	System.out.println(lines.get(0)+"\n");
-	for (int i=1; i<lines.size(); i++) {
-		System.out.println(lines.get(i));
-	}
-		
-		System.out.println("\nChoose From Menu:");
-		System.out.print("1. Manually add a book order\n2. Manually remove a book order\n3. Print an In-Order list of book names by order ID\n4. Find the name of the book for a specific order number\n5. Find the oldest book order\n6. Find the latest book order\n");
-		Scanner scan = new Scanner(System.in);
-		int input;
-		do {
-			while(!scan.hasNextInt()) {
-				System.out.println("Please choose an option 1-6");
-				scan.next();
-			}
-			input = scan.nextInt();
-		} while(input <= 0 || input >=7);
-		
-		switch(input) {
-		case 1:
-			System.out.println("You chose option 1");
-			break;
-			
-		case 2:
-			System.out.println("You chose option 2");
-			break;
-			
-		case 3:
-			System.out.println("You chose option 3");
-			break;
-			
-		case 4:
-			System.out.println("You chose option 4");
-			break;
-			
-		case 5:
-			System.out.println("You chose option 5");
-			break;
-			
-		case 6:
-			System.out.println("You chose option 6");
-			break;
-			
-		}
-	}
+    private static int countNodes(AVLTree.Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return 1 + countNodes(x.left) + countNodes(x.right);
+    }
+
+    // Function to calculate the maximum height of the AVL Tree
+    private static int calculateHeight(AVLTree.Node x) {
+        return (int) Math.ceil(Math.log(countNodes(x) + 1) / Math.log(2));
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("C:\\Users\\aholl91\\eclipse-workspace\\ProgAssign2\\src\\orders.csv"));
+            reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                int orderID = Integer.parseInt(values[0].trim());
+                String title = values[1].trim();
+                tree.setRoot(tree.insert(tree.getRoot(), orderID, title));
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error finding file: " + e.getMessage());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("\nChoose From Menu:");
+                System.out.print(
+                        "1. Manually add a book order\n2. Manually remove a book order\n3. Print an In-Order list of book names by order ID\n4. Find the name of the book for a specific order number\n5. Find the oldest book order\n6. Find the latest book order\n");
+
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Please enter a valid integer choice");
+                    scanner.next();
+                    continue;
+                }
+
+                int choice = scanner.nextInt();
+
+                if (choice < 1 || choice > 7) {
+                    System.out.println("Please enter a valid integer choice");
+                    continue;
+                }
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("You chose to input a new order.");
+                        System.out.println("Enter order ID: ");
+                        if (!scanner.hasNextInt()) {
+                            System.out.println("Please enter a valid integer choice");
+                            scanner.next();
+                        }
+                        int newOrderID = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("Enter book title");
+                        String newTitle = scanner.nextLine();
+                        tree.setRoot(tree.insert(tree.getRoot(), newOrderID, newTitle));
+                        break;
+
+                    case 2:
+                        System.out.println("You chose to remove an order.");
+                        System.out.println("Enter order ID to remove: ");
+                        if (!scanner.hasNextInt()) {
+                            System.out.println("Please enter a valid integer choice");
+                            scanner.next();
+                            continue;
+                        }
+                        int removeOrderID = scanner.nextInt();
+                        tree.setRoot(tree.deleteNode(tree.getRoot(), removeOrderID));
+                        break;
+
+                    case 3:
+                        tree.inOrder(tree.getRoot());
+                        break;
+
+                    case 4:
+                        System.out.println("Enter order ID to search:");
+                        if (!scanner.hasNextInt()) {
+                            System.out.println("Please enter a valid integer choice");
+                            scanner.next();
+                            continue;
+                        }
+                        int searchOrderID = scanner.nextInt();
+                        System.out.println("Book Name: " + tree.search(tree.getRoot(), searchOrderID));
+                        break;
+
+                    case 5:
+                        System.out.println("Oldest Order: " + tree.findOldestOrder(tree.getRoot()).title);
+                        break;
+
+                    case 6:
+                        System.out.println("Latest Order: " + tree.findLatestOrder(tree.getRoot()).title);
+                        break;
+
+                    case 7:
+                        System.exit(0);
+
+                }
+
+                System.out.println("Number of nodes: " + countNodes(tree.getRoot()));
+                System.out.println("Max height: " + calculateHeight(tree.getRoot()));
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                scanner.next();
+            }
+
+        }
+
+    }
+
 }
